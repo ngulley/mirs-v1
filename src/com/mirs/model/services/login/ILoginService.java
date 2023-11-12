b83af748062581c3ad5f71b11409946ec11460ef
ngulley-at-566736454519 <nategulley1969@gmail.com>
2023-11-12 16:52:09 -0600

    update LoginService

--- src/com/mirs/model/services/login/ILoginService.java
+++ src/com/mirs/model/services/login/ILoginService.java
@@ -1,8 +1,26 @@
 package com.mirs.model.services.login;
 
+import com.mirs.model.domain.RentalComposite;
+
 import com.mirs.model.domain.User;
 import com.mirs.model.services.IService;
+import com.mirs.model.services.exception.LoginException;
 
 public interface ILoginService extends IService {
-    public boolean authenticateUser(User user);
+    public final String NAME = "ILoginService";
+    /**
+     * Authenticate customer into our application
+     * @param RentalComposite
+     * 							contains customer information to be authenticated
+     * @throws LoginException
+     * 							If the Customer object in the composite is null or
+     *   				        If the file that contains the customer information is not found or
+     *   					    If an unexpected exception is encountered while accessing the file.
+     * @return boolean
+     * 					true - if authenticated
+     * 				    false - if failed to authenticate
+     * */
+    public boolean authenticateCustomer(RentalComposite rentalComposite) throws LoginException;
+
+    public boolean authenticateAdmin(User user) throws LoginException;
 }
--- src/com/mirs/model/services/login/LoginServiceImpl.java
+++ src/com/mirs/model/services/login/LoginServiceImpl.java
@@ -1,16 +1,30 @@
 package com.mirs.model.services.login;
 
+import com.mirs.model.domain.RentalComposite;
 import com.mirs.model.domain.User;
-import com.mirs.model.services.IService;
+import com.mirs.model.services.exception.LoginException;
 
 public class LoginServiceImpl implements ILoginService {
     /**
-     * Validates if the user is indeed registered in our system.
-     *
-     *  @param user contains a user's login credentials
-     */
-    public boolean authenticateUser(User user) {
+     * Authenticate customer into our application
+     * @param RentalComposite
+     * 							contains customer information to be authenticated
+     * @throws LoginException
+     * 							If the Customer object in the composite is null or
+     *   				        If the file that contains the customer information is not found or
+     *   					    If an unexpected exception is encountered while accessing the file.
+     * @return boolean
+     * 					true - if authenticated
+     * 				    false - if failed to authenticate
+     * */
+    public boolean authenticateCustomer(RentalComposite rentalComposite) throws LoginException {
         System.out.println ("Entering method LoginServiceImpl::authenticateUser");
+
+        return true;
+    }
+
+    public boolean authenticateAdmin(User user) throws LoginException {
+        System.out.println ("Entering method LoginServiceImpl::authenticateAdmin");
 
         return true;
     }
